0dab8e1312cbcd4097b7b326a3584256bb3df91d
Mashal <zyadomar112@gmail.com>
2025-03-09 20:19:46 +0200

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const {AuthRoutes , ApplicationRoutes , FeedbackRoutes , JobPostRoutes} = require("./Routes/routes");
+const { AuthRoutes, ApplicationRoutes, FeedbackRoutes, JobPostRoutes } = require("./Routes/routes");
 
 app.get('/hello', (req, res) => res.send('Hello World!'))
 
@@ -22,9 +22,7 @@ app.use(JobPostRoutes);
 
 const port = process.env.PORT;
 mongoose.connect(process.env.MONGO_URL).then(() => {
-    app.listen(port, () => {
-      console.log(`http://localhost:${port}`);
-    });
-  }).catch((err) => {
-    console.log(err);
-  });
+  app.listen(port);
+}).catch((err) => {
+  console.log(err);
+});
